918f981bdf870881ae696a7356894efdf89426bf
starr-dusT <starrtyler88@gmail.com>
2018-04-04 19:31:28 -0700

    continue work on assignment 2

--- ASSIGNMENTS/ASSIGNMENT_2/main.c
+++ ASSIGNMENTS/ASSIGNMENT_2/main.c
@@ -6,61 +6,65 @@
 #define FREQ_24_MHz 4
 #define FREQ_48_MHz 5
 
-void main(void)
+
+
+
+void delay_ms(float time_ms, int freq)
 {
-    WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;     // stop watchdog timer
-
-    //Unlock Clock Registers
-    CS ->KEY = CS_KEY_VAL;
-
-    //sets MCLK to 12MHz with DCO
-    CS ->CTL0 = 0;
-    CS ->CTL0 |= CS_CTL0_DCORSEL_3 ;
-
-    //sets ACLK to 32.768kHz with REFO and SMCLK 6MHz to with DCO
-    CS ->CTL1 = CS_CTL1_SELM__DCOCLK|CS_CTL1_SELS__DCOCLK|CS_CTL1_SELA__REFOCLK|CS_CTL1_DIVS1;
-
-    //Re-lock Clock Registers
-    CS ->KEY = 0;
-
-    //Blink LED
-    P1 ->SEL0 &= -BIT0;
-    P1 ->SEL1 &= -BIT1;
-    P1 ->DIR |= BIT0;
-    while (1){
-        int i;
-        P1 ->OUT ^= BIT0;
-        for (i = 50000; i > 0; i--);
-    }
-
+    int numCycles = (int)(time_ms*freq*50);
+    int cycles;
+    for (cycles = numCycles; cycles > 0; cycles--);
+    return;
 }
 
-void set_DCO(int FREQ)
+void set_DCO(int freq)
 {
     CS ->KEY = CS_KEY_VAL;
     CS ->CTL0 = 0;
-    switch(FREQ){
+    switch(freq){
         case 0:
-            CS ->CTL1 |= CS_CTL0_DCORSEL_0;
+            CS ->CTL0 |= CS_CTL0_DCORSEL_0;
             break;
         case 1:
-            CS ->CTL1 |= CS_CTL0_DCORSEL_1;
+            CS ->CTL0 |= CS_CTL0_DCORSEL_1;
             break;
         case 2:
-            CS ->CTL1 |= CS_CTL0_DCORSEL_2;
+            CS ->CTL0 |= CS_CTL0_DCORSEL_2;
             break;
         case 3:
-            CS ->CTL1 |= CS_CTL0_DCORSEL_3;
+            CS ->CTL0 |= CS_CTL0_DCORSEL_3;
             break;
         case 4:
-            CS ->CTL1 |= CS_CTL0_DCORSEL_4;
+            CS ->CTL0 |= CS_CTL0_DCORSEL_4;
             break;
         case 5:
-            CS ->CTL1 |= CS_CTL0_DCORSEL_5;
+            CS ->CTL0 |= CS_CTL0_DCORSEL_5;
             break;
         default:
             break;
     }
+    CS ->CTL1 |= CS_CTL1_SELM__DCOCLK;
     CS ->KEY = 0;
     return;
 }
+
+void main(void)
+{
+    WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;     // stop watchdog timer
+    //Blink LED
+    set_DCO(2);
+    P1 ->SEL0 &= -BIT0;
+    P1 ->SEL1 &= -BIT0;
+    P1 ->DIR |= BIT0;
+
+    P4 ->SEL0 |= BIT3;
+    P4 ->SEL1 &= -BIT3;
+    P4 ->DIR |= BIT3;
+
+    while (1){
+       P1 ->OUT ^= BIT0;
+       delay_ms(10, 6);
+    }
+}
+
+
