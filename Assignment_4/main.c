7d5f2852a08e18c5ce0c694581d1401035fa99a6
unknown <izzygazali@yahoo.com>
2018-04-14 16:08:49 -0700

    edited main.c

--- Assignment_4/main.c
+++ Assignment_4/main.c
@@ -20,29 +20,3 @@ void main(void)
     //Print key to LCD, turn multicolor LED on.
     KEYPAD_TO_LCD_LED();
 }
-/*
-int i;
-for(i=0; i<30; i++){
-    KEYPAD_TO_LCD();
-    if(i == 15){
-        LCD_newLine();
-    }
-}
-*/
-
-
-/*
-P2 -> SEL0 &= ~(BIT0 + BIT1 + BIT2);
-P2 -> SEL1 &= ~(BIT0 + BIT1 + BIT2);
-P2 -> DIR  |= (BIT0 + BIT1 + BIT2);
-uint8_t key;
-while(1){
-    key = KEYPAD_GET_KEY();
-    if (key != NO_KEY_PRESS)
-    {
-        P2->OUT = (key & (BIT0 + BIT1 + BIT2));
-        Clear_LCD();
-        KEYPAD_TO_LCD();
-    }
-}
-*/
