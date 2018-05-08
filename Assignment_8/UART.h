129b5f1efddd96ddd788676db53258f8bc459f6e
unknown <izzygazali@yahoo.com>
2018-05-07 17:43:20 -0700

    Assignment 8

--- Assignment_8/UART.h
+++ Assignment_8/UART.h
@@ -6,3 +6,7 @@ uint8_t get_value_flag(void);
 uint16_t get_DAC_level(void);
 void UART_init(void);
 void EUSCIA0_IRQHandler(void);
+void UART_cursor_home(void);
+void UART_clear_console(void);
+void UART_write_command(unsigned char inputString[]);
+void UART_write_string(unsigned char inputString[]);
