ded6f8db623140152d0f9c96cd4a686398defb2d
starr-dusT <starrtyler88@gmail.com>
2018-05-14 00:34:40 -0700

    k lol

--- Tests/Single Channel Test/dmm_functions.c
+++ Tests/Single Channel Test/dmm_functions.c
@@ -1,7 +1,8 @@
  #include "dmm_functions.h"
 
-volatile uint16_t adc_value;
+volatile uint16_t adc_value[101];
 volatile uint16_t adc_index = 0;
+volatile uint32_t adc_sum = 0;
 
 void init_clock(void)
 {
@@ -17,8 +18,13 @@ void init_ADC(void)
     P5->SEL1 |= AIN;
     NVIC->ISER[0] = 1 << ((ADC14_IRQn) & 31);
     __enable_irq();
-    ADC14->CTL0 = ADC14_CTL0_SHT0_0 | ADC14_CTL0_ON | ADC14_CTL0_SHP | ADC14_CTL0_CONSEQ_2 | ADC14_CTL0_ENC;
-    ADC14->CTL1 = ADC14_CTL1_RES_3;
+    ADC14->CTL0 = ADC14_CTL0_SHT0__192 |
+                  ADC14_CTL0_ON |
+                  ADC14_CTL0_SHP |
+                  ADC14_CTL0_CONSEQ_2 |
+                  ADC14_CTL0_MSC |
+                  ADC14_CTL0_DIV__8;
+    ADC14->CTL1 = ADC14_CTL1_RES_1;
     ADC14->MCTL[0] |= ADC14_MCTLN_INCH_15;
     ADC14->IER0 |= ADC14_IER0_IE0;
     return;
@@ -31,10 +37,10 @@ void ADC14_IRQHandler(void)
 {
     if (ADC14->IFGR0 & ADC14_IFGR0_IFG0)
         if (adc_index < 100){
-            adc_value = ADC14->MEM[0];
+            adc_value[adc_index] = ADC14->MEM[0];
             adc_index++;
         }else{
-            //adc_index = 0;
+            adc_index = 0;
         }
 }
 
--- Tests/Single Channel Test/main.c
+++ Tests/Single Channel Test/main.c
@@ -7,8 +7,11 @@ void main(void){
 
     init_clock();
     init_ADC();
+    ADC14->CTL0 |= ADC14_CTL0_ENC | ADC14_CTL0_SC;
 
-    while(1);
+    while(1){
+        __delay_cycles(200000);
+    }
 
 
 }
