844b64ea22b02d7c8df58fab101ce0554f68e388
starr-dusT <starrtyler88@gmail.com>
2018-05-14 21:51:45 -0700

    lol

---
+++ Tests/RMS/dmm_functions.h
@@ -0,0 +1,24 @@
+#include "msp.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+#define AIN BIT4
+
+
+
+void init_ADC(void);
+void init_clock(void);
+
+void init_sample_timer(void);
+
+
+
+
+
+
+
+
+
+
+
+
